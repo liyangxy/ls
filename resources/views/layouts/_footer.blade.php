a18d0b21ea6fe8729af0c10e5aec136d71b04cf9
liyang <673740768@qq.com>
2019-04-11 16:05:58 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      网站所有权归 Dayang 所有
+    </p>
+
+    <!-- <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p> -->
+  </div>
+</footer>
